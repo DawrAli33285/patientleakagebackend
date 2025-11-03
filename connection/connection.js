b93e9b39a326c69a4ff2b9ec0635b409021db5cc
DawrAli33285 <lemightyeagle@gmail.com>
2025-11-03 12:14:38 -0800

    new

--- connection/connection.js
+++ connection/connection.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-const connect=mongoose.connect('mongodb://127.0.0.1/patientleakage')
+const connect=mongoose.connect('mongodb+srv://user:user@cluster0.pfn059x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 
 module.exports=connect;
